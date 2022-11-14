3215e7980319ad2e1df4c5a845b77e9efbd14633
Randy Lynch <71863389+RandArtz@users.noreply.github.com>
2022-11-14 22:38:51 +0000

    Basic stuff

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
-const Discord = require("discord.js");
-const { prefix, token, verify_code } = require("./config.json");
-const fs = require("fs");
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-const client = new Discord.Client();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+// EVENTS HANDLER
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// COMMANDS HANDLER
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+//LOGIN
 client.login(token);
 
-// Web Server
+//WEB SERVER
+// var http = require('http');
 
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end("ok");
-});
-server.listen(3000);
-
-// Bot Activity Status
-
-client.once("ready", () => {
-  console.log("I'm ready!");
-
-  client.user.setPresence({
-    status: "online",
-    activity: {
-      name: "randartz.cf | f?help",
-      type: "PLAYING"
-    }
-  });
-});
+// http.createServer(function (req, res) {
+//   res.write("I'm alive");
+//   res.end();
+// }).listen(8080);
